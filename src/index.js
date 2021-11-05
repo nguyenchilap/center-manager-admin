8a9ee1280a034739d959b45002a8dff190bb5bfb
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2021-11-05 11:37:25 +0700

    setup layout

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const route = require('./routes'); 
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Add static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+//app.use(express.json);
+
+//Template Engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+//Route init
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})

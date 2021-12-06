4b3662751f8dfec6d7def176fc6c01b41bd6d02e
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2021-12-06 09:26:11 +0700

    fix bug

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const adminController = require('../app/controllers/adminController');
+const adminController = require('../app/controllers/AdminController');
 
 //define route
 router.post('/create-account', adminController.createAccount);
